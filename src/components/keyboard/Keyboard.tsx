4004246a48a0e45f09d70e6b8d340fd563cee80a
rsuperman2 <44119296+rsuperman2@users.noreply.github.com>
2022-03-01 16:49:17 -0800

    Update Keyboard.tsx

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -75,7 +75,7 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
       <div className="flex justify-center">
         <Key width={65.4} value="ENTER" onClick={onClick}>
          {t('enterKey')}
-         //{ENTER_TEXT} 
+         
         </Key>
         {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
           <Key
@@ -88,7 +88,7 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
         ))}
         <Key width={65.4} value="DELETE" onClick={onClick}>
          {t('deleteKey')}
-         //{DELETE_TEXT}
+       
         </Key>
       </div>
     </div>
