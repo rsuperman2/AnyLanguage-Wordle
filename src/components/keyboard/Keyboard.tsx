f7e2018429b70eed307f79967dd8f8f82b6eb7cb
rsuperman2 <44119296+rsuperman2@users.noreply.github.com>
2022-03-03 16:15:24 -0800

    Update Keyboard.tsx

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -127,7 +127,7 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
           {t('enterKey')}
         </Key>
         {ORTHOGRAPHY.slice(
-          Math.floor(ORTHOGRAPHY.length * 0.7)+2,
+          Math.floor(ORTHOGRAPHY.length * 0.7)-3,
           ORTHOGRAPHY.length
         ).map((char) => (
           <Key
